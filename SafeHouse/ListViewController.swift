c595fc58fcfc5d6f66f160acd69259b352a20e53
Ryan Voong <ryanvoong@gmail.com>
2015-11-15 23:39:44 -0800

    Display dummy sensor data in TableView

---
+++ SafeHouse/ListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ListViewController.swift
+//  SafeHouse
+//
+//  Created by Ryan Voong on 11/15/15.
+//  Copyright © 2015 Team SafeHouse. All rights reserved.
+//
+
+import UIKit
+
+class Sensor {
+    var ID: String
+    var type: String
+    var status: Int
+    
+    init(ID: String, type: String, status: Int) {
+        self.ID = ID
+        self.type = type
+        self.status = status
+    }
+}
+
+class ListViewController: UITableViewController {
+    // temporary hard-coded data
+    var sensorData: [Sensor] = [
+        Sensor(ID: "Front Door", type: "Door Sensor", status: 0),
+        Sensor(ID: "Window", type: "Window Sensor", status: 1),
+        Sensor(ID: "Back Door", type: "Door Sensor", status: 1)
+    ]
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sensorData.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ListViewCell", forIndexPath: indexPath)
+        cell.textLabel?.text = "\(sensorData[indexPath.row].ID) (\(sensorData[indexPath.row].type))"
+        cell.detailTextLabel?.text = String(sensorData[indexPath.row].status)
+        return cell
+    }
+}
